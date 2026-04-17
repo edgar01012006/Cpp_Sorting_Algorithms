43aedd40160441c56df21d31a44e710375cef5f9
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2026-04-17 20:13:56 +0400

    updated counting sort

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -16,30 +16,46 @@ void print_vector_size(const std::vector<int>& nums) {
 }
 
 void counting_sort(std::vector<int>& nums) {
-    auto [min_elem_it, max_elem_it] = std::minmax_element(nums.begin(), nums.end());
+    auto [min, max] = std::minmax_element(nums.begin(), nums.end());
 
-    auto min_elem = *min_elem_it;
-    auto max_elem = *max_elem_it;
+    int min_el = *min;
+    int max_el = *max;
 
-    auto frequency_map_size = std::abs(min_elem) + std::abs(max_elem) + 1;
+    std::vector<int> count(max_el - min_el + 1);
 
-    std::vector<int> frequency_map(frequency_map_size, 0);   
-
-    for (size_t i{}; i < nums.size(); ++i) {
-        ++frequency_map[nums[i] - min_elem];
+    // print_vector(count);
+    // print_vector_size(count);
+    
+    for (int n: nums) { 
+        ++count[n - min_el];
     }
 
-    for (size_t i{}, start{}; i < frequency_map_size; ++i) {
-        while (frequency_map[i]--) {
-            nums[start++] = i + min_elem;
-        }
+    // print_vector(count);
+    // print_vector_size(count);
+
+    for (int i{1}; i < count.size(); ++i) {
+        count[i] += count[i - 1];
     }
+
+    // print_vector(count);
+    // print_vector_size(count);
+
+    std::vector<int> out(nums.size());
+
+    for (int i = nums.size() - 1; i >= 0; --i) {
+        out[--count[nums[i] - min_el]] = nums[i];
+    }
+
+    // print_vector(out);
+    // print_vector_size(out);
+
+    nums = std::move(out);
 }
 
 int main() {
 
-    std::vector<int> nums { -6, 3, 48, 48, 7, 15, 2 };
-    // std::vector<int> nums { 5, 1, 1, 2, 0, 0 };
+    // std::vector<int> nums { -6, 3, 48, 48, 7, 15, 2 };
+    std::vector<int> nums { -3, -4, 5, 1, 1, 2, 0, 0, -4, -20 };
 
     print_vector(nums);
     print_vector_size(nums);
